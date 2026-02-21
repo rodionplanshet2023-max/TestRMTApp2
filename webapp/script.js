91e42be28c4d3a8eed118de917bf8b7f2ad0ddc1
rodionplanshet2023-max <rodion.planshet2023@gmail.com>
2026-02-21 09:32:52 +0700

    Delete webapp directory

--- webapp/script.js
+++
@@ -1,7 +0,0 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
-
-function sendData() {
-    tg.sendData(JSON.stringify({action: "test", message: "Привет от мини-приложения!"}));
-    tg.showAlert("Данные отправлены!");
-}
